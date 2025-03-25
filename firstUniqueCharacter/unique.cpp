d38a385ccaa2fb6193b80b61cbd4e9e08075ed71
Caelum <caelum.ellis@outlook.com>
2025-03-25 23:35:27 +1100

    Update unique.cpp
    realised easy improvement

--- firstUniqueCharacter/unique.cpp
+++ firstUniqueCharacter/unique.cpp
@@ -6,16 +6,18 @@
 std::size_t firstUniqueChar(const std::string& inputString) {
   // write your solution here
   std::unordered_map<char, int> cnt;
-
-  for(std::size_t i = 0; i < inputString.size(); i++ ){
+  
+  size_t strSize = inputString.size();
+  
+  for(std::size_t i = 0; i < strSize; i++ ){
     char num = inputString[i];
     cnt[num]++;
   }
 
-  for(std::size_t i = 0; i < inputString.size(); i++ ){
+  for(std::size_t i = 0; i < strSize; i++ ){
     if(cnt[inputString[i]] == 1){
       return i;
     }
   }
-  return inputString.size();
+  return strSize;
 }
